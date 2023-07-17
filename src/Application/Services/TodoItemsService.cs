a6448b34237398f670602edd9690fe61c947fdc2
Matthew Parker <matthewparker@hotmail.com.au>
2023-07-17 22:51:03 +1000

    Todo item boilerplate

---
+++ src/Application/Services/TodoItemsService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+
+namespace Application.Services;
+
+public class TodoItemsService
+{
+    public TodoItem GetTodoItem(int id)
+    {
+        return new TodoItem
+        {
+            Id = id,
+            Title = "Todo Item 1"
+        };
+    }
+}
---
+++ src/WebAPI/Routes/TodoItemRoutes.cs
@@ -0,0 +1,22 @@
+﻿using Application.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace WebAPI.Routes;
+
+public static class TodoItemRoutes
+{
+    public static void MapTodoItemRoutes(this WebApplication app)
+    {
+        var group = app.MapGroup("").WithTags("TodoItems").RequireAuthorization().WithOpenApi();
+
+        group
+            .MapGet(
+                "/todoitems",
+                Ok<string> (TodoItemsService todoItemsService, HttpContext context) =>
+                {
+                    return TypedResults.Ok(todoItemsService.GetTodoItem(1));
+                }
+            )
+            .WithName("TodoItems");
+    }
+}
